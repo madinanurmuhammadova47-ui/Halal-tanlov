303f6e44c5fbc4a8ea680aeac258208dcb61aee7
madinanurmuhammadova47-ui <madinanurmuhammadova47@gmail.com>
2026-07-14 15:23:02 +0500

    Change TOKEN to string format
    Updated token format to use string literal.

--- Bot.py
+++ Bot.py
@@ -1,8 +1,8 @@
-8789562762:AAGIVDY8yeS4rThv8mLlXh7ZUGwnwlhVIiM
+
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
-TOKEN = 8851090613:AAEvbhwSKabmboIHxb0kIhVSaAw1KKolcVs
+TOKEN = "8851090613:AAEvbhwSKabmboIHxb0kIhVSaAw1KKolcVs"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Salom!")
