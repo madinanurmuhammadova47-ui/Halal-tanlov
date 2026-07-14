c2526ab3781610059866fac844ae2cceb3202286
madinanurmuhammadova47-ui <madinanurmuhammadova47@gmail.com>
2026-07-14 14:21:24 +0500

    Update Bot.py
    Bot ishlayapti

--- Bot.py
+++ Bot.py
@@ -1,1 +1,13 @@
 8789562762:AAGIVDY8yeS4rThv8mLlXh7ZUGwnwlhVIiM
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
+
+TOKEN = "BU_YERGA_YANGI_TOKEN"
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Salom!")
+
+app = Application.builder().token(TOKEN).build()
+app.add_handler(CommandHandler("start", start))
+
+app.run_polling()
